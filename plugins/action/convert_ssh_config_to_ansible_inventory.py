0dd0a6b234ba9fb0bb4b212c213fdb169aec8b0d
Ben Thomasson <bthomass@redhat.com>
2017-06-06 13:51:55 -0400

    Add support for groups by type in inventory

--- plugins/action/convert_ssh_config_to_ansible_inventory.py
+++ plugins/action/convert_ssh_config_to_ansible_inventory.py
@@ -1,5 +1,6 @@
 from ansible.plugins.action import ActionBase
 
+import yaml
 import paramiko.config
 
 
@@ -15,20 +16,33 @@ class ActionModule(ActionBase):
         ssh_config = paramiko.config.SSHConfig()
         ssh_config_file = self._task.args.get('ssh_config', None)
         output_file = self._task.args.get('output', None)
+        data_file = self._task.args.get('data', None)
+        groups = self._task.args.get('groups', '').split(',')
         with open(ssh_config_file) as f:
             ssh_config.parse(f)
 
+        with open(data_file) as f:
+            data = yaml.load(f.read())
+
+        devices = {device.get('name'): device for device in data.get('devices', [])}
+
         with open(output_file, 'w') as f:
-            f.write("[hosts]\n")
-            for name in ssh_config.get_hostnames():
-                if name != '*':
-                    data = ssh_config.lookup(name)
-                    f.write(" ".join([name] +
-                                     ["=".join(x) for x in [["ansible_host", data['hostname']],
-                                                            ["ansible_port", data['port']],
-                                                            ["ansible_user", data['user']],
-                                                            ["ansible_ssh_private_key_file", data['identityfile'][0]],
-                                                            ]]))
-                    f.write("\n")
+            for group in groups:
+                group = group.strip()
+                f.write("[{0}]\n".format(group))
+                for name in ssh_config.get_hostnames():
+                    if name == '*':
+                        continue
+                    if name not in devices:
+                        continue
+                    if devices[name].get('type') == group:
+                        data = ssh_config.lookup(name)
+                        f.write(" ".join([name] +
+                                         ["=".join(x) for x in [["ansible_host", data['hostname']],
+                                                                ["ansible_port", data['port']],
+                                                                ["ansible_user", data['user']],
+                                                                ["ansible_ssh_private_key_file", data['identityfile'][0]],
+                                                                ]]))
+                        f.write("\n")
 
         return result
