acb5e0af61fbd6bd52fba5ec6d75e722a5afffd0
Ben Thomasson <bthomass@redhat.com>
2017-06-13 10:34:50 -0400

    Add option to use nat in add_cli_connection

--- plugins/action/add_cli_connection.py
+++ plugins/action/add_cli_connection.py
@@ -16,6 +16,8 @@ class ActionModule(ActionBase):
         output_file = self._task.args.get('output', None)
         ssh_config = paramiko.config.SSHConfig()
         ssh_config_file = self._task.args.get('ssh_config', None)
+        use_nat = self._task.args.get('use_nat', False)
+        ssh_key = self._task.args.get('ssh_key', '')
 
         with open(ssh_config_file) as f:
             ssh_config.parse(f)
@@ -25,11 +27,19 @@ class ActionModule(ActionBase):
 
         for device in data['devices']:
             host_data = ssh_config.lookup(device['name'])
-            device['cli'] = dict(host=host_data['hostname'],
-                                 port=int(host_data['port']),
-                                 username=host_data['user'],
-                                 ssh_keyfile=host_data['identityfile'][0],
-                                 transport='cli')
+            if use_nat:
+                device['cli'] = dict(host=device['nat_ip'],
+                                     port=device['nat_port'],
+                                     ssh_keyfile=str(ssh_key),
+                                     transport='cli',
+                                     username=host_data['user'])
+
+            else:
+                device['cli'] = dict(host=host_data['hostname'],
+                                     port=int(host_data['port']),
+                                     username=host_data['user'],
+                                     ssh_keyfile=str(ssh_key) or host_data['identityfile'][0],
+                                     transport='cli')
 
         with open(output_file, 'w') as f:
             f.write(yaml.safe_dump(data, default_flow_style=False))
