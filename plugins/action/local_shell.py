00737733454c407fadc87e44bf38baf292eee75f
Ben Thomasson <bthomass@redhat.com>
2017-08-07 20:18:06 +0000

    Add chdir option to local_shell

--- plugins/action/local_shell.py
+++ plugins/action/local_shell.py
@@ -14,7 +14,10 @@ class ActionModule(ActionBase):
         result = super(ActionModule, self).run(tmp, task_vars)
         command = self._task.args.get('command', None)
         chdir = self._task.args.get('chdir', None)
-        subprocess.check_call(command, shell=True, cwd=os.path.abspath(os.path.join(os.getcwd(), chdir)))
+        cwd = os.getcwd()
+        if chdir:
+            cwd=os.path.abspath(os.path.join(cwd, chdir))
+        subprocess.check_call(command, shell=True, cwd=cwd)
         return result
 
 
